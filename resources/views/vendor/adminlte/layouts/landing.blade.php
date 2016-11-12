2ec160e4f2b67fb8292d8a5d7feaf6e0b6abf517
gusTkd <gus.mac.tkd@gmail.com>
2016-11-12 02:19:54 -0600

    pagina

--- resources/views/vendor/adminlte/layouts/landing.blade.php
+++ resources/views/vendor/adminlte/layouts/landing.blade.php
@@ -83,24 +83,24 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
                 </ol>
                 <div class="carousel-inner">
                   <div class="item active">
-                    <img src="http://placehold.it/900x500/39CCCC/ffffff&amp;text=I+Love+Bootstrap" alt="First slide">
+                    <img src="/img/slide1.png" alt="First slide">
 
                     <div class="carousel-caption">
-                      First Slide
+
                     </div>
                   </div>
                   <div class="item">
-                    <img src="http://placehold.it/900x500/3c8dbc/ffffff&amp;text=I+Love+Bootstrap" alt="Second slide">
+                    <img src="/img/slide2.png" alt="Second slide">
 
                     <div class="carousel-caption">
-                      Second Slide
+
                     </div>
                   </div>
                   <div class="item">
-                    <img src="http://placehold.it/900x500/f39c12/ffffff&amp;text=I+Love+Bootstrap" alt="Third slide">
+                    <img src="/img/slide3.png" alt="Third slide">
 
                     <div class="carousel-caption">
-                      Third Slide
+
                     </div>
                   </div>
                 </div>
