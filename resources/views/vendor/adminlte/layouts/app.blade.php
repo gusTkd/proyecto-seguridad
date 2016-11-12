7f1dfd78358e12b521d1d33434e0bf9deca29966
gusTkd <gus.mac.tkd@gmail.com>
2016-11-11 22:31:10 -0600

    pagina inicio plataforma

--- resources/views/vendor/adminlte/layouts/app.blade.php
+++ resources/views/vendor/adminlte/layouts/app.blade.php
@@ -44,8 +44,105 @@ desired effect
 
         <!-- Main content -->
         <section class="content">
-            <!-- Your Page Content Here -->
-            @yield('main-content')
+          <div class="row">
+            <div class="col-md-12 text-center">
+              <img class="" src="{{ asset('/img/logo-login.png') }}" alt="">
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6">
+              <div class="box box-primary">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Agregados recientemente</h3>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <strong><i class="fa fa-file margin-r-5"></i> Seguridad computacional</strong>
+                      <p class="text-muted">
+                        Segundo examen parcial<br>
+                        <strong>Profesor:</strong> Javier Rosas Hernández
+                      </p>
+                    </div>
+                    <div class="col-md-6 text-center">
+                      <img class="" src="{{ asset('/img/examen.jpg') }}" alt="">
+                    </div>
+                  </div>
+                  <hr>
+
+                  <div class="row">
+                    <div class="col-md-6">
+                      <strong><i class="fa fa-suitcase margin-r-5"></i> Administracion de redes</strong>
+                      <p class="text-muted">
+                        Proyecto Final<br>
+                        <strong>Profesor:</strong> Sergio Quiroz Almaraz
+                      </p>
+                    </div>
+                    <div class="col-md-6 text-center">
+                      <img class="" src="{{ asset('/img/word.jpg') }}" alt="">
+                    </div>
+                  </div>
+                  <hr>
+
+                </div>
+              <!-- /.box-body -->
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="box">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Ranking de participantes</h3>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                  <table class="table table-bordered">
+                    <tbody>
+                      <tr>
+                        <th>Usuario</th>
+                        <th>Aportes</th>
+                        <th>Likes</th>
+                      </tr>
+                      <tr>
+                        <td>Manuel Hidalgo</td>
+                        <td>5</td>
+                        <td>16</td>
+                      </tr>
+                      <tr>
+                        <td>Zurcaled</td>
+                        <td>15</td>
+                        <td>14</td>
+                      </tr>
+                      <tr>
+                        <td>El_Bueno</td>
+                        <td>1</td>
+                        <td>10</td>
+                      </tr>
+                      <tr>
+                        <td>Miguel.1899</td>
+                        <td>3</td>
+                        <td>5</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer clearfix">
+                  <ul class="pagination pagination-sm no-margin pull-right">
+                    <li><a href="#">«</a></li>
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">»</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
         </section><!-- /.content -->
     </div><!-- /.content-wrapper -->
 
